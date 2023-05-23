a4b33514a95fc8233fb3efc2a5c5173878e0a1ab
Daniel Hernandez <daniel0808soares@gmail.com>
2023-05-23 18:18:52 -0300

    set_teclado_digital_letras

--- src/app/termo/termo.component.ts
+++ src/app/termo/termo.component.ts
@@ -9,7 +9,7 @@ import { TermoService } from '../service/termo.service';
 })
 export class TermoComponent implements OnInit {
 
-  constructor(private termo_service: TermoService) { }
+  constructor(private termo_service: TermoService, private elRef: ElementRef) { }
   resposta: string = '';
   tentativa_atual: number = 0;
   tentativas: any[] = [];
@@ -29,23 +29,235 @@ export class TermoComponent implements OnInit {
   @HostListener('document:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     if (event.key === 'Backspace') {
-      for (let i = 0; this.tentativas.length > i; i++) {
-
-        const ultmElPreenc = this.tentativas[i].findLastIndex((element: string) => element != '');
-
-        if (ultmElPreenc != -1) {
-          this.tentativas[i][ultmElPreenc] = '';
-
-          setTimeout(() => {
-            let input = document.getElementById(`tentativa-${i}-letra-${ultmElPreenc}`) as HTMLElement;
-            input?.focus();
-          });
-        }
-      }
+      this.backspaceUltEl();
     }
 
     if (event.key === 'Enter') {
-      this.resposta = "";
+      this.enterTentativa();
+    }
+  }
+
+  ngOnInit(): void {
+
+    this.getPalavras();
+
+  }
+
+  ngAfterViewInit(): void {
+
+    // this.lst_inputs.changes.subscribe(() => {
+    //   const firstElement = this.lst_inputs.first;
+    //   if (firstElement) {
+    //     firstElement.nativeElement.focus();
+    //     this.lst_inputs_sub.unsubscribe();
+    //   }
+    // });
+    // console.log();
+  }
+
+  // ngOnDestroy(): void {
+  //   this.lst_inputs_sub.unsubscribe();
+  // }
+
+  
+
+  getPalavras() {
+    this.tentativas = [];
+    this.letras_palavra = [];
+    this.termo_service.getFormasDePagamentos().subscribe((lst_palavras: any) => {
+      this.palavra_sorteada = this.sorteiaPalavra(lst_palavras.lst_plavras_normais.filter((p: string) => p.length == 5)).toLocaleUpperCase();
+      this.letras_palavra = this.palavra_sorteada.split('');
+
+      for (let i = 0; 5 >= i; i++) {
+        let t: any[] = [];
+        this.letras_palavra.forEach(() => {
+          t.push('')
+        });
+        this.tentativas.push(t);
+      }
+    })
+  }
+
+  inputChange(event: any, tecla_click:string, indexTentativa: number, indexLetra: number) {
+    let inputValue;
+    if(event){
+      inputValue = event.target.value;
+    }else{
+      inputValue = tecla_click;
+    }
+
+    const avanca_input = () => {
+      let proximo_input = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra + 1}`) as HTMLInputElement;
+      if (proximo_input) {
+        proximo_input.value = '';
+        setTimeout(() => {
+          proximo_input?.focus();
+
+        });
+      }
+    }
+
+    if(inputValue == 'backspace'){
+      this.backSpaceClick(indexTentativa, indexLetra);
+      return
+    }
+
+    if (!this.validarLetra(inputValue)) {
+
+      this.tentativas[indexTentativa][indexLetra] = '';
+      this.acionaFocusAtual(indexTentativa, indexLetra);
+
+    } else {
+
+      this.tentativas[indexTentativa][indexLetra] = inputValue.toUpperCase();
+
+      setTimeout(() => {
+        if(event){
+          if (event.inputType == "insertText") {
+            avanca_input();
+          }
+        }else if(tecla_click){
+          avanca_input();
+        }
+
+      });
+    }
+
+    if (inputValue.length > 1) {
+      this.tentativas[indexTentativa][indexLetra] = this.tentativas[indexTentativa][indexLetra][1];
+    }
+
+
+    // const tentativas = JSON.parse(JSON.stringify(this.tentativas));
+    // this.tentativas = JSON.parse(JSON.stringify(tentativas));
+
+  }
+
+  onKeyDownInputFocus(event: KeyboardEvent, indexTentativa: number, indexLetra: number) {
+
+    const volta_input = () => {
+      let input_anterior = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra - 1}`) as HTMLInputElement;
+      if (input_anterior) {
+        input_anterior?.focus();
+      }
+    }
+
+    if (event.key === 'Backspace') {
+      this.msgAviso = "";
+      setTimeout(() => {
+        let input_atual = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra}`) as HTMLInputElement;
+
+        if (input_atual.value == '' && (indexLetra - 1) >= 0) {
+          volta_input();
+
+        } else {
+          input_atual?.focus();
+        }
+      });
+    }
+  }
+
+  sorteiaPalavra(lst_palavras: string[]): string {
+    return lst_palavras[Math.floor(Math.random() * lst_palavras.length)];
+  }
+
+  validarLetra(letra: string): boolean {
+    const regex = /^[a-zA-Z ]+$/; // aceita apenas letras sem acentos e espaços
+    if (regex.test(letra)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  handleMouseDown(event: MouseEvent) {
+    const input = document.querySelector('input') as HTMLElement;
+    if (!input.contains(event.target as Node)) {
+      event.preventDefault();
+    }
+  }
+
+  acionaFocusAtual(indexTentativa: number, indexLetra: number) {
+    setTimeout(() => {
+      let input_atual = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra}`) as HTMLInputElement;
+      input_atual.focus();
+    });
+  }
+
+  setLetra(letra_click:string){
+    const regex = /tentativa-(\d+)-letra-(\d+)/;
+    const elemento_focus = this.elRef.nativeElement.querySelector(':focus');
+    const resultado = regex.exec(elemento_focus?.id);
+    if (resultado) {
+      const tentativa = Number(resultado[1]);
+      const letra = Number(resultado[2]);
+      
+      this.inputChange(null, letra_click, tentativa, letra);
+
+    } else if(letra_click == 'backspace'){
+      this.backspaceUltEl();
+    }else if(letra_click == 'ENTER'){
+      this.enterTentativa()
+    }
+  }
+
+  backSpaceClick(indexTentativa: number, indexLetra: number) {
+    const volta_input = () => {
+      let input_anterior = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra - 1}`) as HTMLInputElement;
+      if (input_anterior) {
+        input_anterior?.focus();
+      }
+    }
+    
+    this.msgAviso = "";
+    this.tentativas[indexTentativa][indexLetra] = '';
+    setTimeout(() => {
+      let input_atual = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra}`) as HTMLInputElement;
+      
+      if (input_atual.value == '' && (indexLetra - 1) >= 0) {
+        volta_input();
+        
+      } else {
+        input_atual?.focus();
+      }
+      });
+  }
+
+  backspaceUltEl(){
+    for (let i = 0; this.tentativas.length > i; i++) {
+
+      const ultmElPreenc = this.tentativas[i].findLastIndex((element: string) => element != '');
+
+      if (ultmElPreenc != -1) {
+        this.tentativas[i][ultmElPreenc] = '';
+
+        setTimeout(() => {
+          let input = document.getElementById(`tentativa-${i}-letra-${ultmElPreenc}`) as HTMLElement;
+          input?.focus();
+        });
+        break;
+      }
+    }
+  }
+
+  focusPriEl(){
+    for (let i = 0; this.tentativas.length > i; i++) {
+
+      const priEl = this.tentativas[i].findIndex((element: string) => element == '');
+
+      if (priEl != -1) {
+
+        setTimeout(() => {
+          let input = document.getElementById(`tentativa-${i}-letra-${priEl}`) as HTMLElement;
+          input?.focus();
+        });
+        break;
+      }
+    }
+  }
+
+  enterTentativa(){
+    this.resposta = "";
 
       const linhaTotalmentePreenchida = this.tentativas[this.tentativa_atual].every((element: string) => element != '');
       const linhaTotalmenteVazia = this.tentativas[this.tentativa_atual].every((element: string) => element == '');
@@ -117,140 +329,9 @@ export class TermoComponent implements OnInit {
 
         this.msgAviso = 'Só palavras com ' + this.tentativas[this.tentativa_atual].length + ' letras';
       }
-    }
   }
+    
 
-  ngOnInit(): void {
 
-    this.getPalavras();
-
-  }
-
-  ngAfterViewInit(): void {
-
-    // this.lst_inputs.changes.subscribe(() => {
-    //   const firstElement = this.lst_inputs.first;
-    //   if (firstElement) {
-    //     firstElement.nativeElement.focus();
-    //     this.lst_inputs_sub.unsubscribe();
-    //   }
-    // });
-    // console.log();
-  }
-
-  // ngOnDestroy(): void {
-  //   this.lst_inputs_sub.unsubscribe();
-  // }
-
-  
-
-  getPalavras() {
-    this.tentativas = [];
-    this.letras_palavra = [];
-    this.termo_service.getFormasDePagamentos().subscribe((lst_palavras: any) => {
-      this.palavra_sorteada = this.sorteiaPalavra(lst_palavras.lst_plavras_normais.filter((p: string) => p.length == 5)).toLocaleUpperCase();
-      this.letras_palavra = this.palavra_sorteada.split('');
-
-      for (let i = 0; 5 >= i; i++) {
-        let t: any[] = [];
-        this.letras_palavra.forEach(() => {
-          t.push('')
-        });
-        this.tentativas.push(t);
-      }
-    })
-  }
-
-  inputChange(event: any, indexTentativa: number, indexLetra: number) {
-    const inputValue = event.target.value;
-
-    const avanca_input = () => {
-      let proximo_input = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra + 1}`) as HTMLInputElement;
-      if (proximo_input) {
-        proximo_input.value = '';
-        setTimeout(() => {
-          proximo_input?.focus();
-
-        });
-      }
-    }
-
-    if (!this.validarLetra(inputValue)) {
-
-      this.tentativas[indexTentativa][indexLetra] = '';
-      console.log(indexTentativa, indexLetra)
-      this.acionaFocusAtual(indexTentativa, indexLetra);
-
-    } else {
-
-      this.tentativas[indexTentativa][indexLetra] = inputValue.toUpperCase();
-
-      setTimeout(() => {
-        if (event.inputType == "insertText") {
-          avanca_input();
-        }
-
-      });
-    }
-
-    if (inputValue.length > 1) {
-      this.tentativas[indexTentativa][indexLetra] = this.tentativas[indexTentativa][indexLetra][1];
-    }
-
-    // const tentativas = JSON.parse(JSON.stringify(this.tentativas));
-    // this.tentativas = JSON.parse(JSON.stringify(tentativas));
-
-  }
-
-  onKeyDownInputFocus(event: KeyboardEvent, indexTentativa: number, indexLetra: number) {
-
-    const volta_input = () => {
-      let input_anterior = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra - 1}`) as HTMLInputElement;
-      if (input_anterior) {
-        input_anterior?.focus();
-      }
-    }
-
-    if (event.key === 'Backspace') {
-      this.msgAviso = "";
-      setTimeout(() => {
-        let input_atual = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra}`) as HTMLInputElement;
-
-        if (input_atual.value == '' && (indexLetra - 1) >= 0) {
-          volta_input();
-
-        } else {
-          input_atual?.focus();
-        }
-      });
-    }
-  }
-
-  sorteiaPalavra(lst_palavras: string[]): string {
-    return lst_palavras[Math.floor(Math.random() * lst_palavras.length)];
-  }
-
-  validarLetra(letra: string): boolean {
-    const regex = /^[a-zA-Z ]+$/; // aceita apenas letras sem acentos e espaços
-    if (regex.test(letra)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  handleMouseDown(event: MouseEvent) {
-    const input = document.querySelector('input') as HTMLElement;
-    if (!input.contains(event.target as Node)) {
-      event.preventDefault();
-    }
-  }
-
-  acionaFocusAtual(indexTentativa: number, indexLetra: number) {
-    setTimeout(() => {
-      let input_atual = document.getElementById(`tentativa-${indexTentativa}-letra-${indexLetra}`) as HTMLInputElement;
-      input_atual.focus();
-    });
-  }
 
 }
